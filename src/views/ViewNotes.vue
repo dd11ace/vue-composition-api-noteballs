b0f28766014c4d945830c2419c3cc53bc0f11510
Gl <Dimadd11ace@yandex.ru>
2022-07-09 02:36:24 +0500

    Notes design

--- src/views/ViewNotes.vue
+++ src/views/ViewNotes.vue
@@ -1,5 +1,19 @@
 <template>
   <div class="notes">
-    <h1>Notes</h1>
+    <div v-for="i in 3" class="card">
+      <div class="card-content">
+        <div class="content">
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quae odio
+          doloribus quaerat officiis commodi, rerum asperiores labore corporis
+          voluptates accusantium omnis molestias. Eos recusandae beatae officia
+          saepe. Harum, recusandae animi?
+        </div>
+      </div>
+      <footer class="card-footer">
+        <a href="#" class="card-footer-item">Save</a>
+        <a href="#" class="card-footer-item">Edit</a>
+        <a href="#" class="card-footer-item">Delete</a>
+      </footer>
+    </div>
   </div>
 </template>
