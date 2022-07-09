2e26ce4c5e2be4a96053d0573dcfc76d08c19ced
Gl <Dimadd11ace@yandex.ru>
2022-07-10 02:54:22 +0500

    Computed (Note Length)

--- src/components/Notes/Note.vue
+++ src/components/Notes/Note.vue
@@ -3,11 +3,14 @@
     <div class="card-content">
       <div class="content">
         {{ note.content }}
+        <div class="has-text-right has-text-grey-light">
+          <small>{{ characterLength }}</small>
+        </div>
       </div>
     </div>
     <footer class="card-footer">
-      <a href="#" class="card-footer-item"></a>
-      <a href="#" class="card-footer-item"></a>
+      <a href="#" class="card-footer-item">Edit</a>
+      <a href="#" class="card-footer-item">Delete</a>
     </footer>
   </div>
 </template>
@@ -15,10 +18,19 @@
 <script setup>
 // props
 
+import { computed } from '@vue/reactivity';
+
 const props = defineProps({
   note: {
     type: Object,
     required: true,
   },
+});
+
+// character length
+const characterLength = computed(() => {
+  let length = props.note.content.length;
+  let description = length > 1 ? 'characters' : 'character';
+  return `${length} ${description}`;
 });
 </script>
