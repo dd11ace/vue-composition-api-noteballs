4db8861a8e00e881599951dfead521ea8f0f048d
Gl <Dimadd11ace@yandex.ru>
2022-07-10 01:30:12 +0500

    Notes Array (Ref)

--- src/views/ViewNotes.vue
+++ src/views/ViewNotes.vue
@@ -16,20 +16,34 @@
       </div>
     </div>
 
-    <div v-for="i in 3" class="card mb-4">
+    <div v-for="note in notes" :key="note.id" class="card mb-4">
       <div class="card-content">
         <div class="content">
-          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quae odio
-          doloribus quaerat officiis commodi, rerum asperiores labore corporis
-          voluptates accusantium omnis molestias. Eos recusandae beatae officia
-          saepe. Harum, recusandae animi?
+          {{ note.content }}
         </div>
       </div>
       <footer class="card-footer">
-        <a href="#" class="card-footer-item">Save</a>
         <a href="#" class="card-footer-item">Edit</a>
         <a href="#" class="card-footer-item">Delete</a>
       </footer>
     </div>
   </div>
 </template>
+
+<script setup>
+// imports
+import { ref } from 'vue';
+
+// notes
+const notes = ref([
+  {
+    id: 'id1',
+    content:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Nobis nihil ullam eaque minus sit eveniet explicabo maxime rerum, architecto similique laborum molestiae ratione consectetur quae. Quibusdam perspiciatis error maxime voluptatem.',
+  },
+  {
+    id: 'id2',
+    content: 'This is a shorter note! Woo!',
+  },
+]);
+</script>
