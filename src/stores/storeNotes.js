1356eb22ac36d6f05d0bcedeb7a3eb920e3012e3
Gl <Dimadd11ace@yandex.ru>
2022-07-11 05:02:43 +0500

    pinia setup

---
+++ src/stores/storeNotes.js
@@ -0,0 +1,19 @@
+import { defineStore } from 'pinia';
+
+export const useStoreNotes = defineStore('storeNotes', {
+  state: () => {
+    return {
+      notes: [
+        {
+          id: 'id1',
+          content:
+            'Lorem ipsum dolor sit amet consectetur adipisicing elit. Nobis nihil ullam eaque minus sit eveniet explicabo maxime rerum, architecto similique laborum molestiae ratione consectetur quae. Quibusdam perspiciatis error maxime voluptatem.',
+        },
+        {
+          id: 'id2',
+          content: 'This is a shorter note! Woo!',
+        },
+      ],
+    };
+  },
+});
